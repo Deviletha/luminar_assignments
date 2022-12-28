189f2011b9d03507cc254b4033651c8fb4f392e8
Deviletha <devicathira1991@gmail.com>
2022-12-28 18:06:05 +0530

    commit

--- lib/Grid with Stack UI/grid_with_stack_ui.dart
+++ lib/Grid with Stack UI/grid_with_stack_ui.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MaterialApp(
     home: Gridstackk(),
   ));
 }
 
-class Gridstackk extends StatelessWidget{
+class Gridstackk extends StatelessWidget {
   var images = [
     "https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&w=1000&q=80",
     "https://media.istockphoto.com/id/1185953092/photo/the-main-attraction-of-paris-and-all-of-europe-is-the-eiffel-tower-in-the-rays-of-the-setting.jpg?b=1&s=170667a&w=0&k=20&c=RQMkDSuO8X0Wm_j6WgvcgR3mTucjAiFFgz0XmeUuYjg=",
@@ -16,42 +17,52 @@ class Gridstackk extends StatelessWidget{
     "https://media.istockphoto.com/id/1214516974/photo/empty-streets-in-dublin-city-centre-during-coronavirus-dublin-ireland.jpg?b=1&s=170667a&w=0&k=20&c=CngzV43wDWKD1SBaYiProiiZtnhJ_cUyaQqfpRQ7hx8="
   ];
 
-  var name = ["USA", "England", "France","Russia","Canada","Dublin"];
+  var name = ["USA", "England", "France", "Russia", "Canada", "Dublin"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("GridView"),),
-        body: GridView.builder(
+      appBar: AppBar(
+        title: Text("GridView"),
+      ),
+      body: GridView.builder(
         gridDelegate:
-        const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-    itemBuilder: (context, index) {
-      return Padding(
-        padding: EdgeInsets.only(left: 10, right: 10,top: 15),
-        child: Stack(
-          children: [Positioned(
-            width: 250,
-            height: 250,
-            child: Card(
-                child: Image(
-                  image: NetworkImage(images[index]),
-                ),
-          ),
-            ),
-            Positioned(
-              width: 250,
-              height: 250,
-                child: Container(
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(left: 10, right: 10, top: 15),
+            child: Stack(
+              children: [
+                Positioned(
                   width: 250,
-                height: 250,
-                child: Text(name[index]),
+                  height: 250,
+                  child: Card(
+                    child: Image(
+                      image: NetworkImage(images[index]),
+                    ),
+                  ),
                 ),
+                Positioned(
+                  width: 250,
+                  height: 250,
+                  child: Container(
+                    width: 250,
+                    height: 250,
+                    child: Text(
+                      name[index],
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          backgroundColor: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
             ),
-        ],
-        ),
-      );
-    },
-          itemCount: images.length,
-    ),
+          );
+        },
+        itemCount: images.length,
+      ),
     );
   }
 }
